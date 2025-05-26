8dd5541507e2893bf8b401c1a4ec93425cd084de
insolor <2442833+insolor@users.noreply.github.com>
2025-05-26 15:25:14 +0300

    Fix broken test workflow (#1)

--- build.zig
+++ build.zig
@@ -4,16 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib_dep = b.dependency("raylib_zig", .{
-        .target = target,
-        .optimize = optimize,
-        .linux_display_backend = .X11,
-    });
-
-    const raylib = raylib_dep.module("raylib"); // main raylib module
-    const raygui = raylib_dep.module("raygui"); // raygui module
-    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
-
     const exe = b.addExecutable(.{
         .name = "game-of-life",
         .root_source_file = b.path("src/main.zig"),
@@ -21,9 +11,27 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-    exe.root_module.addImport("raygui", raygui);
+    var noraylib: bool = false;
+    if (b.args) |args| {
+        if (std.mem.eql(u8, args[0], "noraylib")) {
+            noraylib = true;
+        }
+    }
+
+    if (!noraylib) {
+        const raylib_dep = b.dependency("raylib_zig", .{
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const raylib = raylib_dep.module("raylib"); // main raylib module
+        const raygui = raylib_dep.module("raygui"); // raygui module
+        const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+        exe.linkLibrary(raylib_artifact);
+        exe.root_module.addImport("raylib", raylib);
+        exe.root_module.addImport("raygui", raygui);
+    }
 
     b.installArtifact(exe);
 
@@ -31,9 +39,9 @@ pub fn build(b: *std.Build) void {
 
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
@@ -42,6 +50,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
+        .test_runner = .{ .path = b.path("test_runner.zig"), .mode = .simple },
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
