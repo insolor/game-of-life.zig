2607db52273ed1b0522f412c3b14c3c84db61ebd
insolor <2442833+insolor@users.noreply.github.com>
2025-05-30 20:41:56 +0300

    Put code from main into App class

--- src/main.zig
+++ src/main.zig
@@ -6,45 +6,61 @@ const Field = models.Field;
 const object_library = @import("object_library.zig");
 const engine = @import("engine.zig");
 
-pub fn main() anyerror!void {
-    const screenWidth = 800;
-    const screenHeight = 600;
-    const frame_skip = 10;
+const App = struct {
+    allocator: std.mem.Allocator,
+    field: Field,
+    display_params: display.DisplayParams,
+    frame_skip: usize = 10,
 
-    const display_params = display.DisplayParams{
-        .width = screenWidth,
-        .height = screenHeight,
-    };
+    const Self = @This();
+    fn init(allocator: std.mem.Allocator, screen_width: usize, screen_height: usize) Self {
+        rl.initWindow(@intCast(screen_width), @intCast(screen_height), "Game of Life");
+        rl.setExitKey(rl.KeyboardKey.null); // Don't exit on Esc key press
+        rl.setTargetFPS(60);
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var field = Field.init(allocator);
-    defer field.deinit();
+        return .{
+            .allocator = allocator,
+            .field = Field.init(allocator),
+            .display_params = .{
+                .width = screen_width,
+                .height = screen_height,
+            },
+        };
+    }
 
-    try field.putObject(object_library.GLIDER, 1, 1);
-    try field.putObject(object_library.SPACESHIP, 1, 10);
+    fn deinit(self: *Self) void {
+        rl.closeWindow();
+        self.field.deinit();
+    }
 
-    rl.initWindow(screenWidth, screenHeight, "Game of Life");
-    defer rl.closeWindow();
+    fn mainLoop(self: *Self) !void {
+        var frame_count: u32 = 0;
+        while (!rl.windowShouldClose()) : (frame_count += 1) {
+            // Draw
+            rl.beginDrawing();
+            defer rl.endDrawing();
 
-    rl.setExitKey(rl.KeyboardKey.null); // Don't exit on Esc key press
+            rl.clearBackground(rl.Color.black);
+            display.displayField(self.field, self.display_params);
 
-    rl.setTargetFPS(60);
-
-    var frame_count: u32 = 0;
-    while (!rl.windowShouldClose()) : (frame_count += 1) {
-        // Draw
-        rl.beginDrawing();
-        defer rl.endDrawing();
-
-        rl.clearBackground(rl.Color.black);
-        display.displayField(field, display_params);
-
-        // Update
-        if (frame_count % frame_skip == 0) {
-            const next_state = try engine.calculateFieldNextState(field);
-            field.deinit();
-            field = next_state;
+            // Update
+            if (frame_count % self.frame_skip == 0) {
+                const next_state = try engine.calculateFieldNextState(self.field);
+                self.field.deinit();
+                self.field = next_state;
+            }
         }
     }
+};
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var app = App.init(allocator, 800, 600);
+    defer app.deinit();
+
+    try app.field.putObject(object_library.GLIDER, 1, 1);
+    try app.field.putObject(object_library.SPACESHIP, 1, 10);
+
+    try app.mainLoop();
 }
