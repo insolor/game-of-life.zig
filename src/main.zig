5b6bcf988fd44026d0bd30486cb6d43e54b7bc9e
insolor <2442833+insolor@users.noreply.github.com>
2025-05-30 22:28:22 +0300

    Implement view panning with middle mouse button (buggy)

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,28 @@ const Field = models.Field;
 const object_library = @import("object_library.zig");
 const engine = @import("engine.zig");
 
+const PanningParams = struct {
+    initial_mouse_x: isize,
+    initial_mouse_y: isize,
+    initial_offset_x: isize,
+    initial_offset_y: isize,
+
+    const Self = @This();
+    fn offsetX(self: Self, mouse_x: isize) isize {
+        return self.initial_mouse_x + mouse_x - self.initial_mouse_x;
+    }
+
+    fn offsetY(self: Self, mouse_y: isize) isize {
+        return self.initial_offset_y + mouse_y - self.initial_mouse_y;
+    }
+};
+
 const App = struct {
     allocator: std.mem.Allocator,
     field: Field,
     display_params: display.DisplayParams,
+    panning_params: ?PanningParams = null,
+
     frame_skip: usize = 10,
     is_running: bool = true,
     step: bool = false,
@@ -66,7 +84,7 @@ const App = struct {
             self.is_running = false;
             self.step = true;
         }
-        
+
         // right - faster
         if (rl.isKeyPressed(rl.KeyboardKey.right)) {
             if (!self.is_running) {
@@ -75,7 +93,7 @@ const App = struct {
                 self.frame_skip = @max(1, self.frame_skip - 1);
             }
         }
-        
+
         // left - slower
         if (rl.isKeyPressed(rl.KeyboardKey.left)) {
             if (!self.is_running) {
@@ -86,12 +104,44 @@ const App = struct {
         }
     }
 
+    fn getMousePosition() struct { x: isize, y: isize } {
+        const mouse_position = rl.getMousePosition();
+        return .{
+            .x = @intFromFloat(mouse_position.x),
+            .y = @intFromFloat(mouse_position.y),
+        };
+    }
+
+    fn panning(self: *Self) void {
+        // Finish view panning on middle mouse button release
+        if (rl.isMouseButtonUp(rl.MouseButton.middle)) {
+            self.panning_params = null;
+            return;
+        }
+
+        const mouse_position = Self.getMousePosition();
+        if (self.panning_params) |panning_params| {
+            // Recalculate offset during the panning
+            self.display_params.pixel_offset_x = panning_params.offsetX(mouse_position.x);
+            self.display_params.pixel_offset_y = panning_params.offsetY(mouse_position.y);
+        } else if (rl.isMouseButtonDown(rl.MouseButton.middle)) {
+            // Start view panning on middle mouse button press
+            self.panning_params = .{
+                .initial_mouse_x = mouse_position.x,
+                .initial_mouse_y = mouse_position.y,
+                .initial_offset_x = self.display_params.pixel_offset_x,
+                .initial_offset_y = self.display_params.pixel_offset_y,
+            };
+        }
+    }
+
     fn run(self: *Self) !void {
         self.initDisplay();
 
         var frame_count: usize = 0;
         while (!rl.windowShouldClose()) : (frame_count += 1) {
             self.runningControls();
+            self.panning();
             self.draw();
             if ((self.is_running or self.step) and frame_count % self.frame_skip == 0) {
                 try self.nextFieldState();
